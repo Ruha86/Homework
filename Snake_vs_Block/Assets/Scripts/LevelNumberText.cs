5bd59d58a31f7a5aec4599784caceb3e06121357
Ruha86 <ruhasheff@gmail.com>
2022-02-04 14:15:23 +0300

    edit
    edit UI
    edit scripts

--- Snake_vs_Block/Assets/Scripts/LevelNumberText.cs
+++ Snake_vs_Block/Assets/Scripts/LevelNumberText.cs
@@ -5,25 +5,12 @@ using UnityEngine.SceneManagement;
 public class LevelNumberText : MonoBehaviour
 {
     public Text LevelNumber;
-    public Text FoodCounter;
-
     public GameState GameState;
-
-    int counter;
 
     void Start()
     {
         Scene scene = SceneManager.GetActiveScene();
         LevelNumber.text = scene.name;
-
-        counter = GetComponent<Player>().foodCounter;
     }
 
-    private void Update()
-    {
-
-        FoodCounter.text = counter.ToString();
-
-        Debug.Log(counter);
-    }
 }
